32eb3ef6fd1045218b20023b4ff5ed8c3143963a
Matias Szeinfeld <matinger@gmail.com>
2013-10-15 13:01:39 -0300

    Actualizo seleccion perfiles

--- src/Beans/SeleccionBean.java
+++ src/Beans/SeleccionBean.java
@@ -6,10 +6,14 @@ import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import Celiacos.Perfil;
+import Celiacos.PerfilSocio;
 import Celiacos.TipoUnidad;
+import Dao.FactoryDAO;
 import Servicios.GestionUnidadesServicio;
 
 @ManagedBean
@@ -17,34 +21,39 @@ import Servicios.GestionUnidadesServicio;
 public class SeleccionBean {
 
 	public SeleccionBean() {}
-	private Map<String, String> mapValue;
-	private String seleccion;
+	private Map<String, Integer> mapValue;
+	private Integer seleccion;
 	
-	public Map<String,String> getMapValue() {
+	public Map<String,Integer> getMapValue() {
 		return mapValue;
 	}
-	public String getSeleccion() {
+	public Integer getSeleccion() {
 		return seleccion;
 	}
-	public void setSeleccion(String seleccion) {
+	public void setSeleccion(Integer seleccion) {
 		this.seleccion = seleccion;
 	}
 	
 	public void cargarPerfiles(){
-		mapValue = new LinkedHashMap<String,String>();
+		mapValue = new LinkedHashMap<String,Integer>();
 		GestionUnidadesServicio unidadesservicio = new GestionUnidadesServicio();
 		FacesContext context = FacesContext.getCurrentInstance();
 		
 		List<Perfil> perfiles = (List<Perfil>) context.getExternalContext().getSessionMap().get("perfiles");
 		System.out.println("Cantidad de perfiles: " + perfiles.size());
 		for (Perfil p : perfiles){
-			mapValue.put(p.getClass().getName(), p.getClass().getName()); //label, value
+			mapValue.put(p.getClass().getName(), p.getId()); //label, value
 		}
 	}
 	
 	public String ingresar() {
 		System.out.println("PerfilSeleccionado: " + seleccion);
-		return seleccion;
+		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
+		HttpSession session =  (HttpSession)ctx.getSession(false);	
+		PerfilSocio ps = FactoryDAO.getPerfilSocioDAO().encontrar(seleccion);
+		session.setAttribute("perfil", ps);
+		System.out.println(ps.getClass().getName());
+		return ps.getClass().getName();
 	}
 	
 }
